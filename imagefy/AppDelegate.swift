54b586ba15c326d08d12a1e8743f2d0f418ac396
Alan <alan@char.home>
2016-05-22 02:26:54 -0300

    [ADD] adjustments in tabbaritems

--- imagefy/AppDelegate.swift
+++ imagefy/AppDelegate.swift
@@ -24,8 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         IQKeyboardManager.sharedManager().enable = true
         
-        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: kAccentColor], forState:.Normal)
-        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.whiteColor()], forState:.Selected)
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: kAccentColor], forState:.Selected)
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.whiteColor()], forState:.Normal)
         
         UINavigationBar.appearance().setBackgroundImage(UIImage(), forBarPosition: .Any, barMetrics: .Default)
         UINavigationBar.appearance().shadowImage = UIImage()
