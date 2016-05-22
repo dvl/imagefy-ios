ae493a7b08a7398d7956230beeaa07cf9e35faaf
Guilherme Assis <guilherme@suamusica.com.br>
2016-05-22 06:14:15 -0300

    Tela de profile finalizada

--- imagefy/Modules/Profile/ProfileViewController.swift
+++ imagefy/Modules/Profile/ProfileViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ProfileViewController: UIViewController {
 
     @IBOutlet weak var imgProfile: UIImageView!
+    @IBOutlet weak var lblUsername: UILabel!
+    @IBOutlet weak var lblEmail: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +21,9 @@ class ProfileViewController: UIViewController {
         let user = appDelegate.loggedUser
         
         imgProfile.loadResourceImageWithUrl(user!.imageUrl, placeholder: UIImage(named: "profilePlaceholder"))
+        lblUsername.text = user?.name
+        lblEmail.text = user?.email
+        
         UIDesign.viewCornerRadius(imgProfile.layer, bounds: imgProfile.bounds, byRoundingCorners: .AllCorners, radius: imgProfile.frame.size.width/2)
     }
 
@@ -27,6 +32,11 @@ class ProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func btnLogoutClick(sender: AnyObject) {
+        NSUserDefaults.standardUserDefaults().removeObjectForKey("TokenKey")
+        let delegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        delegate.setupInitialView()
+    }
 
     /*
     // MARK: - Navigation
